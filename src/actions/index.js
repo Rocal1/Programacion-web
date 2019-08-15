d83aa1c521997f5e7baf3e55a66e28c3d6b7bb0b
Rodrigo Calabretta <rodrigocalabretta@gmail.com>
2019-08-15 01:18:01 -0300

    Seteando la API para apuntar a la API hosteada

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,8 @@ export function showUsers() {
     
     return (dispatch, getState) => {
         //axios.get('http://jsonplaceholder.typicode.com/users')
-        //axios.get('https://rickywep.pythonanywhere.com/loshermanos/endpoint/api/Articulo.json')
-        axios.get('http://127.0.0.1:8000/losHermanos/endpoint/api/Articulo.json')
+        axios.get('https://rickywep.pythonanywhere.com/loshermanos/endpoint/api/Articulo.json')
+        //axios.get('http://127.0.0.1:8000/losHermanos/endpoint/api/Articulo.json')
             .then((response) => {
                 dispatch( { type: SHOW_USERS, payload: response.data.contenido } ) 
             })
