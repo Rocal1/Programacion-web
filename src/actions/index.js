573782c2518e373b4ab81ef477d0f05c65cc1488
Rodrigo Calabretta <rodrigocalabretta@gmail.com>
2019-08-15 01:01:56 -0300

    Inicializando la repo en git

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export const SHOW_USERS = 'SHOW_USERS'
+
+export function showUsers() {
+    
+    return (dispatch, getState) => {
+        //axios.get('http://jsonplaceholder.typicode.com/users')
+        //axios.get('https://rickywep.pythonanywhere.com/loshermanos/endpoint/api/Articulo.json')
+        axios.get('http://127.0.0.1:8000/losHermanos/endpoint/api/Articulo.json')
+            .then((response) => {
+                dispatch( { type: SHOW_USERS, payload: response.data.contenido } ) 
+            })
+    }
+    
+} 
+
---
+++ src/components/app.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Component } from 'react';
+import { connect } from 'react-redux'
+import { showUsers } from '../actions'
+import { Table } from 'react-bootstrap'
+
+class App extends Component {
+  
+  componentWillMount() {
+    this.props.showUsers()
+  }
+  // function() {
+  //   return {data: {users:[]}}
+  // }
+  
+  renderUsersList() {
+    return this.props.users.map((user) => {
+      return (
+        <tr key={user.id}>
+          <td>{user.id}</td>
+          <td>{user.nombre}</td>
+          <td>{user.tipo}</td>
+        </tr>
+      )
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <h2>Users List</h2>
+        <Table responsive>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.renderUsersList() }
+          </tbody>
+        </Table>        
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    users: state.user.list
+  }
+}
+
+export default connect(mapStateToProps, { showUsers })(App)
